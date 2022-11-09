b89dd1a0f0e8c3ccbed6487f867fefbd93e1b779
Benjamin Wilson <whatsup@btwicode.com>
2022-11-09 15:16:45 -0700

    server init

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.send("Successful response.");
+});
+
+const port = process.env.S_PORT || 3000;
+
+app.listen(port, () =>
+  console.log(`Example app is listening on port ${port}.`)
+);
